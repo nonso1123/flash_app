6649350e39ac5c8bf10781c3f06ac06c6585bf94
nonso1123 <chinonsonwora@gmail.com>
2025-01-01 10:32:11 +0100

    commit

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
 	};
 
 	useEffect(() => {
-		isMounted = true; // Track if component is mounted
+		isMounted = false; // Track if component is mounted
 		fetchData();
 
 		return () => {
